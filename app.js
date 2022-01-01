81ff03692082133fe452c2b5857ceea08e1322ba
SANKALP1011 <tigerishan426@gmail.com>
2022-01-01 16:28:45 +0530

    Routes added , app structure and wokrflow created.

--- app.js
+++ app.js
@@ -3,3 +3,33 @@ const ejs = require("ejs");
 const mysql = require("mysql");
 const app = express();
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+  res.render("Home")
+});
+app.post("/",function(req,res){
+
+});
+app.get("/Login",function(req,res){
+  res.render("Login");
+})
+app.get("/SignIn",function(req,res){
+  res.render("SignIn")
+});
+app.post("/SignIn",function(req,res){
+  res.redirect("/Insert");
+});
+app.get("/Insert",function(req,res){
+  res.render("Insert");
+});
+app.post("/Insert",function(req,res){
+  console.log(req.body.Name);
+  res.render("Insert");
+});
+app.get("/Database",function(req,res){
+  res.render("Database")
+});
+app.listen("3000",function(){
+    console.log("server and uo running");
+})
