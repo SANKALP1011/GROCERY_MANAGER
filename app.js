1dac2a7b1fb8d149451d7c038425444b5b466b65
SANKALP1011 <tigerishan426@gmail.com>
2022-01-05 23:00:04 +0530

    Commnet added , readibilty improved.

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+/* Requiring express module */
 const express = require("express");
+/* Requiring ejs module */
 const ejs = require("ejs");
+/* Requiring mysql module */
 const mysql = require("mysql");
+/* creating app */
 const app = express();
+/* Express embeded body-parser */
 app.use(express.urlencoded({ extended: true }));
+/* Adding static file */
 app.use(express.static(__dirname + "/public"));
+/* Setting ejs as the view engine */
 app.set("view engine", "ejs");
 
-var dbConfig = ({
+/* Coonecting to local mysql database using the credentials*/
+var dbConfig = {
   host: "us-cdbr-east-05.cleardb.net",
-  user: "b9339d8e6ad7e8" ,
-  password: "f5a0438b" ,
-  database: "heroku_82e0818920d6a5b" 
-});
-    /* To run on your local machine */
-//Connection on the local machiene-: 
+  user: "b9339d8e6ad7e8",
+  password: "f5a0438b",
+  database: "heroku_82e0818920d6a5b",
+};
+
+/* To run on your local machine */
+//Connection on the local machiene-:
 // var coonection = mysql.createConnection({
 //   host: "127.0.0.1",
 //   user: "root",
@@ -22,43 +31,45 @@ var dbConfig = ({
 
 /* This handles the error when the database connection is lost */
 function handleDisconnect() {
-  coonection = mysql.createConnection(dbConfig);         //Creating and recreating the connection when the connection is lost or 
-  coonection.connect(function(err) {               // The server is either down
-    if(err) {                                           //  or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000);              // We introduce a delay before attempting to reconnect and solve the issue.
+  coonection = mysql.createConnection(dbConfig); //Creating and recreating the connection when the connection is lost or
+  coonection.connect(function (err) {
+    // The server is either down
+    if (err) {
+      //  or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect and solve the issue.
+    } else {
+      console.log("Database connected successfully"); //This lines shows if the database is connected successfully.
     }
-    else{
-      console.log("Database connected successfully");   //This lines shows if the database is connected successfully.
-    }                                  
-  }); 
-coonection.on('error', function(err) {
-  console.log('db error', err);
-  if(err.code === 'PROTOCOL_CONNECTION_LOST') {        // If it gets the following error(when coonection is lost), then
-    handleDisconnect();                                // We will call the function and restart the connection again.
-  } else {                                             // Otherwise throw the error and then again the call the function.
-    throw err;                                         // Handles error
-  }
-});
+  });
+  coonection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // If it gets the following error(when coonection is lost), then
+      handleDisconnect(); // We will call the function and restart the connection again.
+    } else {
+      // Otherwise throw the error and then again the call the function.
+      throw err; // Handles error
+    }
+  });
 }
-handleDisconnect();
+handleDisconnect();  //Calling the function again to fix the server lost issue.
 
-
+/* Home Route */
 app.get("/", function (req, res) {
   res.render("Home");
 });
-
 app.post("/", function (req, res) {
   //Nothing for the post route.
 });
 
+/* Login Route */
 app.get("/Login", function (req, res) {
   res.render("Login");
 });
-
 app.post("/Login", function (req, res) {
   var email = req.body.Email;
-  console.log(email);
+  //Quering the databse for the alredy regidtered user.
   var Login = "select* from Users where Email = ?";
   coonection.query(Login, [email], function (err, result) {
     if (result.length != 0) {
@@ -70,6 +81,8 @@ app.post("/Login", function (req, res) {
     }
   });
 });
+
+/* Sign In Route */
 app.get("/SignIn", function (req, res) {
   res.render("SignIn");
 });
@@ -78,6 +91,7 @@ app.post("/SignIn", function (req, res) {
   var FullName = req.body.FullName;
   var UserName = req.body.CompanyName;
   var Password = req.body.Password;
+  //Adding the user to the database.
   var Register =
     "Insert into Users(FullName,UserName,Email,Password) VALUES ('" +
     FullName +
@@ -97,9 +111,8 @@ app.post("/SignIn", function (req, res) {
     }
   });
 });
-app.post("/SignIn", function (req, res) {
-  res.redirect("/Insert");
-});
+
+/* Insert Route */
 app.get("/Insert", function (req, res) {
   res.render("Insert");
 });
@@ -110,6 +123,7 @@ app.post("/Insert", function (req, res) {
   var Type = req.body.Type;
   var Qty = req.body.Qty;
   var Price = req.body.Price;
+  //Adding the items to the database.
   var Add =
     "Insert into Items(Id,Name,Type,Qty,Price) VALUES ('" +
     Id +
@@ -131,7 +145,10 @@ app.post("/Insert", function (req, res) {
   });
   res.redirect("/Database");
 });
+
+/* Database Route */
 app.get("/Database", function (req, res) {
+  //Fetching information from the database.
   var data = "select * from Items";
   coonection.query(data, function (err, result, data) {
     if (err) {
@@ -144,8 +161,8 @@ app.get("/Database", function (req, res) {
   });
 });
 app.get("/Delete/:ID", function (req, res) {
+  //Deleting the value from database using the id.
   var id = req.params.ID;
-  console.log(id);
   var query = "Delete from Items where id = ?";
   coonection.query(query, [id], function (err, data) {
     if (err) {
@@ -156,17 +173,18 @@ app.get("/Delete/:ID", function (req, res) {
     res.redirect("/Database");
   });
 });
+/* Search Route */
 app.get("/Search", function (req, res) {
   res.render("Search");
 });
 app.post("/Search", function (req, res) {
+  //Seaching the data which the user want from the database.
   console.log(req.body.SearchedText);
   var SearchedName = req.body.SearchedText;
   var SearchResult = "Select * from Items where Name = ?";
   coonection.query(SearchResult, [SearchedName], function (err, result) {
     if (err) {
       console.log(err);
-      console.log("Please search the corrcet item and check your spellings");
     } else {
       for (var i = 0; i < result.length; i++) {
         res.render("Result", {
@@ -180,6 +198,7 @@ app.post("/Search", function (req, res) {
     }
   });
 });
-app.listen( process.env.PORT ||"3001", function () {
-  console.log("server and uo running");
+/* Initialising the server */
+app.listen(process.env.PORT || "3001", function () {
+  console.log("server and up running");
 });
